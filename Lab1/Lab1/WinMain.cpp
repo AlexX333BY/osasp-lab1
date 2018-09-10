21034c2b7e526862667fcde2295d70b1e0a8235f
Aliaksei <alexx333by@gmail.com>
2018-09-10 12:06:33 +0300

    Implemented exit message handler

---
+++ Lab1/Lab1/WinMain.cpp
@@ -0,0 +1,55 @@
+#include <windows.h>
+
+#define WND_CLASS_NAME "LaboratoryWork1Class"
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_KEYDOWN:
+		if (wParam != VK_ESCAPE)
+		{
+			break;
+		}
+	case WM_CLOSE:
+		if (MessageBox(hWnd, "Are you sure you want to exit?", "Exit question", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		{
+			PostQuitMessage(0);
+		}
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wndClassEx;
+	wndClassEx.cbSize = sizeof(WNDCLASSEX);
+	wndClassEx.style = CS_DBLCLKS;
+	wndClassEx.lpfnWndProc = WndProc;
+	wndClassEx.cbClsExtra = 0;
+	wndClassEx.cbWndExtra = 0;
+	wndClassEx.hInstance = hInstance;
+	wndClassEx.hIcon = NULL;
+	wndClassEx.hCursor = NULL;
+	wndClassEx.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClassEx.lpszMenuName = NULL;
+	wndClassEx.lpszClassName = WND_CLASS_NAME;
+	wndClassEx.hIconSm = NULL;
+	RegisterClassEx(&wndClassEx);
+
+	HWND hWnd = CreateWindow(WND_CLASS_NAME, "Lab 1", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
